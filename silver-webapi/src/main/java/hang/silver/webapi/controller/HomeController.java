997ea5851c4046d4653d425625cc89826a1af6af
ljh <ljinghang@yeah.net>
2017-10-20 16:22:33 +0800

    显示API在线文档，访问 http://localhost:8080/swagger-ui.html

--- silver-webapi/src/main/java/hang/silver/webapi/controller/HomeController.java
+++ silver-webapi/src/main/java/hang/silver/webapi/controller/HomeController.java
@@ -4,13 +4,18 @@ import hang.silver.common.ApiResult;
 import hang.silver.common.ApiResultCode;
 import hang.silver.repo.UserInfo;
 import hang.silver.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Api(value = "100 首页", tags = {"首页"})
+@RequestMapping("api/home")
 @RestController
 public class HomeController extends BaseController {
 
 	@GetMapping("/")
+	@ApiOperation(value = "获取用户信息", notes = "这里是说明")
 	public ApiResult<UserInfo> get(){
 		UserService service = new UserService();
 		UserInfo userInfo = service.get();
